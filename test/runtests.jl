cf60791db8d35063f6985a3a387c428aba2379b0
Alexandre Bergel <alexandre.bergel@me.com>
2021-09-08 09:47:18 -0300

    plenty of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,25 @@ out = plusTwo(2)
 
 out = plusTwo(5)
 @test out == 7
+
+
+@test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 2) == [1, 2, 30, 40]
+@test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 1) == [1, 20, 30, 40]
+@test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 0) == [10, 20, 30, 40]
+@test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 4) == [1, 2, 3, 4]
+
+@test checkIfSimilar([1, 2, 3, 4], mutateAtIndex([1, 2, 3, 4], 2))
+@test checkIfSimilar([1, 2, 3, 4], mutateAtIndex([1, 2, 3, 4], 1))
+@test checkIfSimilar([1, 2, 3, 4], mutateAtIndex([1, 2, 3, 4], 4))
+
+@test fitness([3, 2, 1, 4, 5]) == 2
+@test fitness(reverse(collect(1:5))) == 4
+@test fitness(1:5) == 0
+
+@test pickBestIndividual([[3, 2, 1, 4, 5], reverse(collect(1:5)), 1:5]) == 1:5
+@test pickBestIndividual([[3, 2, 1, 4, 5], reverse(collect(1:5))]) == [3, 2, 1, 4, 5]
+
+@test selectIndividual([[3, 2, 1, 4, 5], reverse(collect(1:5)), 1:5]) == 1:5
+@test selectIndividual([[3, 2, 1, 4, 5], reverse(collect(1:5))]) == [3, 2, 1, 4, 5]
+
+@test run(maxNumberOfIterations=40)[1] = [1, 2, 3, 4, 5]
