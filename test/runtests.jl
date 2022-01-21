6807902ae1a4ebead6ff8e81ebe98d863634bfa8
Alexandre Bergel <alexandre.bergel@me.com>
2022-01-21 12:32:13 +0100

    cleaning the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,5 @@
 using Test, GeneticAlgorithm
 
-out = plusTwo(2)
-@test out == 4
-
-out = plusTwo(5)
-@test out == 7
-
-
 @test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 2) == [1, 2, 30, 40]
 @test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 1) == [1, 20, 30, 40]
 @test crossoverAtIndex([1, 2, 3, 4], [10, 20, 30, 40], 0) == [10, 20, 30, 40]
